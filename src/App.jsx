c002000a9d4d087764d097a92764d819f6ee7d08
uchechi <isuuchechi@gmail.com>
2024-01-09 11:55:57 +0100

    cloud managent

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-
+import React from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
 
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+    </div>
+  )
+}
+
+export default App;
