75a1caafb697be334c03d1ca63ea0bd4ebf037f5
uchechi <isuuchechi@gmail.com>
2024-01-11 11:43:09 +0100

    cloud management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Support from './pages/Support'
+import Platform from './pages/Platform'
+import Pricing from './pages/Pricing'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    
+     <Router>
+      <Navbar/>
+      <Routes>
+       <Route path='/'element={<Home/>} />
+       <Route path='/about'element={<About/>} />
+       <Route path='/support'element={<Support/>} />
+       <Route path='/platform'element={<Platform/>} />
+       <Route path='/pricing'element={<Pricing/>} />
+       <Route path='/login'element={<Login/>} />
+       <Route path='/register'element={<Register/>} />
+      </Routes>
+     </Router>
+    
   )
 }
 
-export default App;
+export default App
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { IoMdClose } from "react-icons/io";
 import { IoMenuSharp } from "react-icons/io5";
 // import logo from './assets/logo-removebg-preview(1).png'
 import logo from '../assets/cloudd.png'
+import { Link } from 'react-router-dom';
 
 
 const Navbar=()=> {
@@ -24,21 +25,24 @@ const Navbar=()=> {
     <div className='w-screen h-[80px] z-10 bg-[#f9f9f9] fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
-          <h1 className='text-4xl font-bold mr-4 sm:text-3xl flex'><img src={logo} 
-          className='w-8 mr-2' alt="img logo" />cloud</h1>
-         
-
+        <Link to={'/'}>
+          <h1 className='text-4xl font-bold mr-4 sm:text-3xl flex'>
+         <img src={logo} className='w-8 mr-2' alt="img logo" />cloud</h1>
+          </Link>
+    
          <ul className='hidden md:flex gap-7 font-bold text-2xi px-40'>
-           <li>Home</li>
-           <li>About</li>
-           <li>Support</li>
-           <li>platform</li>
-           <li>pricing</li>
+
+        <Link to={'/'}><li>Home</li></Link>
+        <Link to={'/about'}><li>About</li></Link>
+        <Link to={'/support'}><li>Support</li></Link>
+        <Link to={'/platform'}><li>Platform</li></Link>
+        <Link to={'/pricing'}><li>Pricing</li></Link>
+           
          </ul>
         </div>
          <div className='hidden md:flex pr-4'>
-          <button className='border-none bg-[#201f1f] text-[#eedfdf] rounded-xl px-5 py-3 mr-4'>Login</button>
-          <button className='border-none bg-[#201f1f] text-[#eedfdf] rounded-xl px-5 py-3 mr-4'>Register</button>
+        <Link to={'/login'}><button className='border-none bg-[#201f1f] text-[#eedfdf] rounded-xl px-5 py-3 mr-4'>Login</button></Link>
+        <Link to={'/register'}><button className='border-none bg-[#201f1f] text-[#eedfdf] rounded-xl px-5 py-3 mr-4'>Register</button></Link> 
          </div>
 
          <div className='md:hidden mr-4' onClick={handlclick}> 
@@ -47,11 +51,11 @@ const Navbar=()=> {
          </div>
       </div>
     <ul className={!nav ? 'hidden': 'absoulte bg-[#f9f9f9] w-[100%] px-8'} onClick={handlclose}>
-      <li className='border-b-2 py-3 border-black-300 w-full'>Home</li>
-      <li className='border-b-2 py-3 border-black-300 w-full'>About</li>
-      <li className='border-b-2 py-3 border-black-300 w-full'>Support</li>
-      <li className='border-b-2 py-3 border-black-300 w-full'>Platform</li>
-      <li className='border-b-2 py-3 border-black-300 w-full'>Pricing</li>
+       <Link to={'/'}><li className='border-b-2 py-3 border-black-300 w-full'>Home</li></Link>
+      <Link to={'/about'}><li className='border-b-2 py-3 border-black-300 w-full'>About</li></Link>
+      <Link to={'/support'}><li className='border-b-2 py-3 border-black-300 w-full'>Support</li></Link>
+       <Link to={'/platform'}><li className='border-b-2 py-3 border-black-300 w-full'>Platform</li></Link>
+     <Link to={'/pricing'}><li className='border-b-2 py-3 border-black-300 w-full'>Pricing</li></Link> 
 
 <div className='flex flex-col my-4'>
   <button className='bg-[#201f1f] text-[#eedfdf] px-8 py-3 mb-4 rounded-2xl'>Login</button>
