9be9b9af00d5edab91f2ed23e698eafce41eb28f
uchechi <isuuchechi@gmail.com>
2024-01-12 11:44:41 +0100

    cloud management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home_innerpage from '../Innerpages/Home_innerpage'
+import Home_innerpagetwo from '../Innerpages/Home_innerpagetwo'
 
 const Home=()=> {
   return (
     <div>
       <Home_innerpage/>
+      <Home_innerpagetwo/>
     </div>
   )
 }
